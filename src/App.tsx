5c457a679cf7ecd4dc6743cbbaf295861165b63c
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-22 19:48:09 -0300

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {Text, View} from 'react-native';
 export const App = () => {
   return (
     <View>
-      <Text>Hola a todos</Text>
+      <Text>Hola a todo el mundo</Text>
     </View>
   );
 };
