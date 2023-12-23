047da5b0ac457efabf0116f9a622b9bbb7ea5e22
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-22 21:29:17 -0300

    codepush app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import {Text, View} from 'react-native';
-
-export const App = () => {
+import codePush from 'react-native-code-push';
+const App = () => {
   return (
     <View>
       <Text>Hola a todo el mundo</Text>
     </View>
   );
 };
+
+export default codePush(App);
