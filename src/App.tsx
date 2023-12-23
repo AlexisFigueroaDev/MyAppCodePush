9e1ba15fbf02ab3a2be2fe0c7dc0d793a35ad124
Alexis Figueroa <agfigueroa@teco.com.ar>
2023-12-23 13:51:49 -0300

    title name

--- src/App.tsx
+++ src/App.tsx
@@ -15,42 +15,21 @@
 //   updateDialog: true,
 //   installMode: codePush.InstallMode.IMMEDIATE,
 // })(App);
-import React, {useState, useEffect} from 'react';
-import {
-  Modal,
-  View,
-  Text,
-  Button,
-  Alert,
-  Platform,
-  ProgressBarAndroid,
-  ProgressViewIOS,
-} from 'react-native';
+
+import React, {useEffect} from 'react';
 import codePush from 'react-native-code-push';
+import {Text, View, Alert} from 'react-native';
 
-const UpdateChecker = () => {
-  const [updateAvailable, setUpdateAvailable] = useState(false);
-  const [downloadProgress, setDownloadProgress] = useState(0);
-
+const App = () => {
   useEffect(() => {
     const checkForUpdates = async () => {
       try {
         const update = await codePush.checkForUpdate();
+        // const update = true;
+
+        console.log(update);
         if (update) {
-          setUpdateAvailable(true);
-
-          const syncStatus = await codePush.sync(
-            {installMode: codePush.InstallMode.IMMEDIATE},
-            status => {
-              if (status === codePush.SyncStatus.DOWNLOADING_PACKAGE) {
-                setDownloadProgress(codePush.getDownloadProgress());
-              }
-            },
-          );
-
-          if (syncStatus === codePush.SyncStatus.UPDATE_INSTALLED) {
-            // La actualización se instaló, puedes reiniciar la aplicación si es necesario
-          }
+          showUpdateDialog(update);
         }
       } catch (error) {
         console.error('Error checking for update:', error);
@@ -60,62 +39,33 @@ const UpdateChecker = () => {
     checkForUpdates();
   }, []);
 
-  const showUpdateDialog = () => {
+  const showUpdateDialog = updateInfo => {
     Alert.alert(
       'Actualización disponible',
       'Una nueva versión está disponible. ¿Quieres actualizar?',
       [
         {
           text: 'Actualizar',
-          onPress: async () => {
-            setUpdateAvailable(false);
-            await codePush.sync({installMode: codePush.InstallMode.IMMEDIATE});
-          },
+          onPress: () =>
+            codePush.sync({installMode: codePush.InstallMode.IMMEDIATE}),
         },
         {
           text: 'Cancelar',
-          onPress: () => {
-            setUpdateAvailable(false);
-          },
+          onPress: () => {},
         },
       ],
     );
   };
 
   return (
-    <Modal
-      transparent={true}
-      animationType="slide"
-      visible={updateAvailable}
-      onRequestClose={() => {
-        setUpdateAvailable(false);
-      }}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{backgroundColor: 'white', padding: 20, borderRadius: 10}}>
-          <Text>¡Hay una actualización disponible!</Text>
-          {downloadProgress > 0 ? (
-            <>
-              <Text>Descargando: {Math.round(downloadProgress * 100)}%</Text>
-              {Platform.OS === 'android' ? (
-                <ProgressBarAndroid
-                  styleAttr="Horizontal"
-                  indeterminate={false}
-                  progress={downloadProgress}
-                  style={{width: 200}}
-                />
-              ) : (
-                <ProgressViewIOS
-                  progress={downloadProgress}
-                  style={{width: 200}}
-                />
-              )}
-            </>
-          ) : null}
-          <Button title="Actualizar" onPress={showUpdateDialog} />
-        </View>
-      </View>
-    </Modal>
+    <View>
+      <Text>hola ale 3</Text>
+    </View>
   );
 };
 
-export default codePush(UpdateChecker);
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.IMMEDIATE,
+  updateDialog: true,
+})(App);
